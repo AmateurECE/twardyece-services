eb35d94dadc48d46e4a6cd94f5b66cc3ec4d9a47
Ethan D. Twardy <ethan.twardy@gmail.com>
2021-08-27 06:48:54 -0500

    Restore rootless status of jenkins-agent image
    Following the hints at devops.stackexchange.com/questions/4540,
    I was able to change a couple of things to allow the jenkins-agent
    image to become rootless yet again. Looks like just changing the
    current user at the time of mountpoint location and adding the
    VOLUME directive...I feel like I should have been able to figure
    that out.

--- jenkins-agent-cmd.sh
+++ jenkins-agent-cmd.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-chown -R jenkins:jenkins $REPOSITORY_MOUNT
-
 CONTROLLER_HOSTNAME=http://edtwardy-webservices_jenkins-controller_1:8080
 JNLP_PATH=jenkins/computer/docker-agent/jenkins-agent.jnlp
-exec su jenkins -c "java -jar /usr/share/jenkins/agent.jar \
+exec /opt/java/openjdk/bin/java -jar /usr/share/jenkins/agent.jar \
      -jnlpUrl $CONTROLLER_HOSTNAME/$JNLP_PATH \
      -secret @/secrets/jnlp-secret \
-     -workDir /home/jenkins/agent"
+     -workDir /home/jenkins/agent
