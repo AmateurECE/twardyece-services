81fa7c96c04b7c27f2f91b4a963f4f77ae9496a0
Ethan D. Twardy <ethan.twardy@gmail.com>
2024-01-09 16:58:02 -0600

    Resolve a few more startup order bugs
    Services were starting before the wireguard interfaces, and services
    were occasionally starting in the wrong order. Now, all is well!

--- declarations.mk
+++ declarations.mk
@@ -49,18 +49,26 @@ clean:
 
 # Creates a symlink from foo.target.requires/bar@baz.service to ../bar@.service
 define addRequiresTemplate
-mkdir -p $(DESTDIR)/$(SYSTEMD_SYSTEM_UNITDIR)/$(1).requires; \
+mkdir -p $(DESTDIR)$(SYSTEMD_SYSTEM_UNITDIR)/$(1).requires; \
 ln -s ../$$(perl -pe 's/(?<=@)[^.]+//' <<<"$(2)") \
-    $(DESTDIR)/$(SYSTEMD_SYSTEM_UNITDIR)/$(1).requires/$(2);
+    $(DESTDIR)$(SYSTEMD_SYSTEM_UNITDIR)/$(1).requires/$(2);
 endef
 
 # Creates a symlink from foo.target.requires/bar.service to ../barservice
 define addRequires
-mkdir -p $(DESTDIR)/$(SYSTEMD_SYSTEM_UNITDIR)/$(1).requires; \
+mkdir -p $(DESTDIR)$(SYSTEMD_SYSTEM_UNITDIR)/$(1).requires; \
 ln -s ../$(2) $(DESTDIR)$(SYSTEMD_SYSTEM_UNITDIR)/$(1).requires/$(2);
 endef
 
+# Create a symlink from foo.target.wants/bar.service to ../bar.service
 define addWants
-mkdir -p $(DESTDIR)/$(SYSTEMD_SYSTEM_UNITDIR)/$(1).wants; \
-ln -s ../$(2) $(DESTDIR)/$(SYSTEMD_SYSTEM_UNITDIR)/$(1).wants/$(2);
+mkdir -p $(DESTDIR)$(SYSTEMD_SYSTEM_UNITDIR)/$(1).wants; \
+ln -s ../$(2) $(DESTDIR)$(SYSTEMD_SYSTEM_UNITDIR)/$(1).wants/$(2);
+endef
+
+# Create a drop-in file that specifies a Before dependency on foo for bar
+define dropinBefore
+mkdir -p $(DESTDIR)$(SYSTEMD_SYSTEM_UNITDIR)/$(1).d; \
+printf '%s\n' "[Unit]" "Before=$(2)" \
+	>$(DESTDIR)$(SYSTEMD_SYSTEM_UNITDIR)/$(1).d/$(2).conf;
 endef
