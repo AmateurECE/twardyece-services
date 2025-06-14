e87fefe3712ef4921365d1f03831b14fcbab30f5
Ethan D. Twardy <ethan.twardy@gmail.com>
2025-06-14 10:08:57 -0500

    dns: Update records to point to CHI server

--- ethantwardy.com.lua
+++ ethantwardy.com.lua
@@ -1,7 +1,7 @@
 -- LuaDNS records for the ethantwardy.com domain
 
-a(_a, "172.105.97.217")
-a("*", "172.105.97.217")
+a(_a, "172.234.20.52")
+a("*", "172.234.20.52")
 a("mail", "172.234.20.52")
 mx(_a, "mail.ethantwardy.com", 10)
 txt(_a, "v=spf1 ip4:172.234.20.52 ip6:fe80::f03c:95ff:fe2f:513a ~all")
--- twardyece.com.lua
+++
@@ -1,4 +0,0 @@
--- LuaDNS records for the twardyece.com domain
-
-a(_a, "172.105.97.217")
-a("*", "172.105.97.217")
