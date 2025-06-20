85b60c2cc80773b982670903a16a20062e349e48
Ethan D. Twardy <ethan.twardy@gmail.com>
2025-06-20 15:35:45 -0500

    dns: Cleanup record names

--- ethantwardy.com.lua
+++ ethantwardy.com.lua
@@ -10,8 +10,8 @@ txt(
 	"v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArubAqWEHD5esibM+NmRbxkFBkHdkO0ltL3gopADNu0/i/BbD3WIIMHXCZwBN3b+BdtHEbhB6wIN6bYTEQadVxmtXg7zcMv5AIe60agRIdNTTX4vscpXFSHCAm8qIRvoBC2bVEI+P8YJAcqVswWgFI3JpU3cI4luwjsNYIb7Sjy/BAJRtp9x+flad3BC6hu5PuV8owIcBg30oExVGPiDDdHFORo5fSfe3E3fmCLqvI+dydSQGeEKNH9zRYOg1nAL7tdJFmq8Gewysz/OOVwf47GMgquMpw97ql/WAj6lsSCIevMpK/s1wCl3guxRqjpGYzKVkrkdmhGOiAha7NDEI5wIDAQAB"
 )
 txt("_dmarc", "v=DMARC1; p=reject; rua=mailto:postmaster@ethantwardy.com;")
-txt("_mta-sts.ethantwardy.com.", "v=STSv1; id=20250619;")
-txt("_smtp._tls.ethantwardy.com.", "v=TLSRPTv1; rua=mailto:postmaster@ethantwardy.com;")
+txt("_mta-sts", "v=STSv1; id=20250619;")
+txt("_smtp._tls", "v=TLSRPTv1; rua=mailto:postmaster@ethantwardy.com;")
 tlsa("_25._tcp", 3, "6a0864b49b44f9acdc71acbb13a5b5fa56fd298afa1613c1c87b66316e411b06")
 
 --- Needed to remain verified in Google Postmaster tools
