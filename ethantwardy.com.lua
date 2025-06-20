c5ef5a42154f74148cdbca21659973dbbfec0ded
Ethan D. Twardy <ethan.twardy@gmail.com>
2025-06-19 21:10:07 -0500

    dns: Add TLS-RPT and MTA-STS records

--- ethantwardy.com.lua
+++ ethantwardy.com.lua
@@ -10,6 +10,8 @@ txt(
 	"v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArubAqWEHD5esibM+NmRbxkFBkHdkO0ltL3gopADNu0/i/BbD3WIIMHXCZwBN3b+BdtHEbhB6wIN6bYTEQadVxmtXg7zcMv5AIe60agRIdNTTX4vscpXFSHCAm8qIRvoBC2bVEI+P8YJAcqVswWgFI3JpU3cI4luwjsNYIb7Sjy/BAJRtp9x+flad3BC6hu5PuV8owIcBg30oExVGPiDDdHFORo5fSfe3E3fmCLqvI+dydSQGeEKNH9zRYOg1nAL7tdJFmq8Gewysz/OOVwf47GMgquMpw97ql/WAj6lsSCIevMpK/s1wCl3guxRqjpGYzKVkrkdmhGOiAha7NDEI5wIDAQAB"
 )
 txt("_dmarc", "v=DMARC1; p=reject; rua=mailto:postmaster@ethantwardy.com;")
+txt("_mta-sts.ethantwardy.com.", "v=STSv1; id=20250619;")
+txt("_smtp._tls.ethantwardy.com.", "v=TLSRPTv1; rua=mailto:postmaster@ethantwardy.com;")
 
 --- Needed to remain verified in Google Postmaster tools
 txt(_a, "google-site-verification=7Gpev4O9vRdmnM4THOpEM9b50byopvngKREYLTdEHTQ")
